702689bee44f76e400ffff1232c09b8466cc5a1f
hiroin <egamescape@gmail.com>
2022-05-16 10:18:49 +0900

    Reduxのデータの永続化とアイコンの設定

--- store/index.tsx
+++ store/index.tsx
@@ -1,11 +1,22 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import userReducer from './reducers/user';
+import { persistReducer, persistStore } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import strage from 'redux-persist/lib/storage';
 
 const rootReducer = combineReducers({
   user: userReducer,
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['user'],
+};
 
+const persistentReducer = persistReducer(persistConfig, rootReducer);
+const store = createStore(persistentReducer, composeWithDevTools());
+
+export const persistor = persistStore(store);
 export default store;
